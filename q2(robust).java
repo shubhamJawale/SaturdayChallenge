84220ada284d942fdc9f5a73316f9002453e91be
shubhamJawale <50077968+shubhamJawale@users.noreply.github.com>
2021-06-12 23:06:21 +0530

    Add files via upload

---
+++ q2(robust).java
@@ -0,0 +1,153 @@
+// q2.java
+import java.util.*;
+class node{
+
+    int data;
+    node next;
+
+    node(int element){
+        data = element;
+        node next = null;
+
+    }
+
+}
+
+
+class LinkedList{
+
+    node head = null;
+    int count =0;
+    int tempcounter = 1;
+    void addElement(int element){
+        node newnode = new node(element);
+        if(head==null)
+            {
+                head = newnode;
+                count ++;
+            }
+            else{
+                node pointer = head;
+                while(pointer.next!=null)
+                {
+                    pointer=pointer.next;
+                }
+                pointer.next = newnode;
+                count ++;
+        }
+        }
+
+
+        void swap(){
+            
+            node prev = null;
+            node nextp= null;
+            node cur= head;
+            node temp=head;
+            if(head==null)
+            {
+                //System.out.println(" ");
+            }
+            else{
+            while(temp.next!=null)
+            {
+                temp=temp.next;
+                
+            }
+                 
+                 //{
+                    for(int i=1; i<count-1; i++)
+                    {
+                        prev=cur;
+                        cur=cur.next;
+                        if(cur!=null){ 
+                          nextp=cur.next;
+                          } 
+                          else{
+                            break;
+                            }
+                       
+                        tempcounter++;
+                        
+                        if(prev==head)
+                        {
+                            prev.next=nextp;
+                            cur.next=prev;
+                            head=cur;
+                            cur=prev;
+                            prev=head;
+
+
+                        }
+                        else{
+                            prev.next=nextp;
+                            cur.next=nextp.next;
+                            nextp.next=cur;
+                           
+                            }
+
+                    }
+                //}
+
+                // }
+            }
+
+
+        }
+
+         void display()
+     {
+
+         if(head == null)
+         {
+             System.out.println("[ ]");
+         }
+         else
+
+
+         {    
+
+             System.out.print(" ");
+             node pointer = head;
+             while(pointer!= null)
+             {
+             
+             System.out.print(" [ "+pointer.data+" ] ");
+
+             pointer = pointer.next;
+             //System.out.print();
+             }
+             
+
+         }
+     }
+}
+
+public class q2{
+    
+
+        public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+        ll.addElement(1);
+        ll.addElement(2);
+        ll.addElement(3);
+        ll.addElement(4);
+        // ll.addElement(5);
+        // ll.addElement(6);
+        // ll.addElement(7);
+        // ll.addElement(8);
+        // ll.addElement(9);
+        // ll.addElement(10);
+       
+        
+        ll.display();
+
+        ll.swap();
+        System.out.println();
+        ll.display();
+
+        
+
+    }
+}
